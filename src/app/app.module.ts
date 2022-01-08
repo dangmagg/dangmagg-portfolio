424b5c1dda9bdb70e6f18684a2f116e7ee8848e3
Maggie Dang <maggiedang11@gmail.com>
2022-01-08 00:30:42 -0500

    Add project component and project pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { MaterialModule } from 'src/global/material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EducationComponent } from './education/education.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 import { TopToolBarComponent } from './top-tool-bar/top-tool-bar.component';
 import { WorkComponent } from './work/work.component';
@@ -21,13 +23,15 @@ import { WorkComponent } from './work/work.component';
     GreetingComponent,
     EducationComponent,
     SkillsComponent,
-    WorkComponent
+    WorkComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface ProjectEntry {
+  title: string;
+  subtitle: string;
+  desc1: string;
+  desc2: string;
+  desc3: string;
+  desc4: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+  configPages: any;
+
+  projs: ProjectEntry[] = [
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a2", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a3", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.configPages = {
+      itemsPerPage: 2,
+      currentPage: 1,
+      totalItems: this.projs.length
+    };
+  }
+
+  onPageChanged(event){
+    this.configPages.currentPage = event;
+  }
+
+}
