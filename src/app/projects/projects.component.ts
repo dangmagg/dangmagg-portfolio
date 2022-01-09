b8a790087a81ff279c04b28f9e59231040073de3
Maggie Dang <maggiedang11@gmail.com>
2022-01-08 19:17:04 -0500

    Adjust project card layout

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -20,11 +20,11 @@ export class ProjectsComponent implements OnInit {
   configPages: any;
 
   projs: ProjectEntry[] = [
-    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
-    {title: "a2", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "e", desc4: "lhfkdlshflksdjgkdsjgfsjg;lsjgdlshflksdjgkdsjgfsjg;lsjg;ljgl;dsjg;ljg;ldsjggjgfsjg;lsjg;ljgl;dsjdlshflksdjgkdsjgfsjg;lsjg;ljgl;dsjg;ljg;ldsjggjgfsjg;lsjg;ljgl;dsj;ljgl;dsjg;ljg;ldsjggjgfsjg;lsjg;ljgl;dsjg;ljg;ldsjgkfjg;lsjg;sjgldjgl;sdjgsdjg;ksdjg;djg;sdjgd;sjgdsjggjgkfjg;lsjg;sjgldjgl;sdjgsdjg;ksdjg;djg;sdjgd;sjg;ldgj;sdljg;lsdjg;lgj;s", link: ""},
+    {title: "a2", subtitle: "b", desc1: "c2", desc2: "", desc3: "e", desc4: "", link: "link here"},
     {title: "a3", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
-    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
-    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "", desc3: "", desc4: "", link: ""},
+    {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: "linkhere"},
     {title: "a", subtitle: "b", desc1: "c", desc2: "d", desc3: "", desc4: "", link: ""},
 
   ];
