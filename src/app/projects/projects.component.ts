7ea99379c4861995a8a46720194fcb016d558d09
Maggie Dang <maggiedang11@gmail.com>
2022-01-10 19:16:26 -0500

    Add more images to project section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -21,14 +21,19 @@ export class ProjectsComponent implements OnInit {
 
   configPages: any;
 
-  /*TODO: Add Github link for portfolio website */
+  /*TODO: Add Image and Github link for portfolio website */
   projs: ProjectEntry[] = [
     {title: "SportCRED Social Media Website", subtitle: "Angular, TypeScript, Django, Python", 
       desc1: "Designed and developed a social media platform for sport enthusiasts in a team of 6 for our client, SportCRED.", 
       desc2: "Chosen as the website the client liked best out of 7+ teams and was utilized as the company’s first functional prototype.", 
       desc3: "Implemented front-end features for users to register, login, access admin privileges, view news, user posts, setting bets, and playing sports trivia.", 
       desc4: "Practiced agile software development lifecycle with daily scrum meetings.", 
-      link: "", linkTitle: "", image: ""},
+      link: "", linkTitle: "", image: "assets/sportcred.gif"},
+    {title: "Farm Citi App", subtitle: "Android Studio, Java, Photoshop", 
+      desc1: "Participated in Hackathon “Hack Lassonde” with a team of 4 to create an android application to visualize the environmental impact of food choices a user makes.", 
+      desc2: "Implemented and designed the main dashboard screen and splash screen.", 
+      desc3: "", desc4: "", link: "https://devpost.com/software/test-3g8sir", linkTitle: "Devpost + Github Link", 
+      image: "assets/farmciti.PNG"},
     {title: "ReAccess UI/UX Prototype", subtitle: "Figma, Photoshop", 
       desc1: "Worked with a team of 6 to research, design, create, and present a prototype of an accessibility software modeled to help the hard-of-seeing and deaf community to navigate the web more effectively.", 
       desc2: "Chosen by group members as the one with the best initial UI/UX design to base the project off on.", 
@@ -38,18 +43,13 @@ export class ProjectsComponent implements OnInit {
     {title: "Personal Portfolio Website", subtitle: "Angular, Typescript", 
       desc1: "Developed and designed this portfolio website meant to showcase personal and work experience to employers.", 
       desc2: "", desc3: "", desc4: "", link: "todo", linkTitle: "Github Link", 
-      image: ""},
+      image: "assets/portfolio.png"},
     {title: "Computer Science Course Website", subtitle: "JavaScript, Python, HTML, CSS, Flask, SQLite", 
-      desc1: "Designed and developed a course website in a team of 2.", 
+      desc1: "Designed and developed a working course website in a team of 2. (Image shown is a just a mockup)", 
       desc2: "Created features to allow users to view course materials, updates from instructors, enter marks, submit remark requests, anonymous feedback, and download files.", 
       desc3: "Implemented register and login features with a factor to distinguish a user as student or admin.", 
       desc4: "Utilized SQLite to store and query student grades, requests, encrypted account data.", 
-      link: "", linkTitle: "", image: ""},
-    {title: "Farm Citi App", subtitle: "Android Studio, Java, Photoshop", 
-      desc1: "Participated in Hackathon “Hack Lassonde” with a team of 4 to create an android application to visualize the environmental impact of food choices a user makes.", 
-      desc2: "Implemented and designed the main dashboard screen and splash screen.", 
-      desc3: "", desc4: "", link: "https://devpost.com/software/test-3g8sir", linkTitle: "Devpost + Github Link", 
-      image: "assets/farmciti.PNG"},
+      link: "", linkTitle: "", image: "assets/courseweb.png"},
     {title: "Scikit-learn Reverse Engineer Project", subtitle: "Python, Visual Paradigm", 
       desc1: "Created new features and implemented bug fixes with a team of 7 students for open-sourced machine learning Python library, Scikit-learn.", 
       desc2: "Led the new feature implementation meant to fetch third party datasets from an external website and allow that data to be processed into consumable formats.", 
