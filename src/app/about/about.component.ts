695afe792e94db66b3c186c1fa4744e4c24a671b
Maggie Dang <maggiedang11@gmail.com>
2022-05-19 18:32:11 -0400

    Update about section and adjust padding

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface About {
+  desc: string;
+}
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+
+  about: About[] = [
+    { desc: "Develop nice, modern, and mobile friendly websites on the full stack (MEAN)" },
+    { desc: "Create Android Mobile Applications with Android Studio" },
+    { desc: "Fluent in using both SQL and NOSQL Database technologies" },
+    { desc: "Ability to integrate third party services; Firebase, AWS, Heroku" },
+    { desc: "Knowledgeable in Computer Security, Artificial Intelligence, and Machine Learning" },
+    { desc: "Work experience in an agile software development lifecycle" }
+  ]
 
   constructor() { }
 
