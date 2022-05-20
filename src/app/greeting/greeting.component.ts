c518438b43bedbb74030fb15fd7ba69fc981ef0f
Maggie Dang <maggiedang11@gmail.com>
2022-05-20 18:58:05 -0400

    Update greeting with profile image

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -11,7 +11,7 @@ export class GreetingComponent implements OnInit {
   greet = {
     fullname: "Maggie Dang",
     title: "Hello, I'm Maggie",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat. Donec eu vehicula neque. Donec viverra lorem nunc, tempus euismod eros sollicitudin ut. Quisque et tincidunt libero. Donec id pharetra justo. Proin euismod lacinia dolor, eu scelerisque justo tempus pharetra. Vivamus nunc justo, finibus ut nisl sed, euismod rhoncus nulla. Proin sed magna egestas, egestas ante et, congue eros. In consequat, mauris dapibus tincidunt suscipit, ex libero aliquet diam, at maximus risus enim non leo."
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus."
   }
 
   constructor() { }
